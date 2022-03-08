83b4dde3da9441da7e3528d125c7c94d05fbe217
deal-machine <95deal@gmail.com>
2022-03-07 21:19:07 -0300

    chore: configure jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,9 @@ export default {
   collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
-  // coverageProvider: "v8",
+  coverageProvider: "v8",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
+  testEnvironment: "node",
 };
