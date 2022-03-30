15e946bb2b7cf5afa5ad65a1f46175b83566ceb2
deal-machine <95deal@gmail.com>
2022-03-30 20:12:12 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,9 +7,9 @@ describe("Signup Controller", () => {
 
     const httpRequest = {
       body: {
-        email: "",
-        password: "",
-        passwordConfirmation: "",
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
       },
     };
 
@@ -17,5 +17,25 @@ describe("Signup Controller", () => {
 
     expect(httpResponse).toHaveProperty("code");
     expect(httpResponse.code).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+  });
+
+  test("Should return 400 if no email is provided", async () => {
+    //system under test
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse).toHaveProperty("code");
+    expect(httpResponse.code).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
   });
 });
