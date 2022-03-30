3f242514cb633269168c839f736c3ff6dd6731b0
deal-machine <95deal@gmail.com>
2022-03-30 20:32:19 -0300

    feat: use http interface and custom error for missing params

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,18 @@
+import { HttpRequest, HttpResponse } from "../protocols/http";
+import { MissingParamError } from "../errors/missing-param-error";
+
 export class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
-        code: 400,
-        body: new Error("Missing param: name"),
+        statusCode: 400,
+        body: new MissingParamError("name"),
       };
     }
     if (!httpRequest.body.email) {
       return {
-        code: 400,
-        body: new Error("Missing param: email"),
+        statusCode: 400,
+        body: new MissingParamError("email"),
       };
     }
   }
