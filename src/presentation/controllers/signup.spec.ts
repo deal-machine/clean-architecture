3ff0d178a11bcd18d90eee3ef3261cc57dc41b2e
deal-machine <95deal@gmail.com>
2022-03-30 21:42:37 -0300

    test: define required fields and implements controller interface

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -38,4 +38,42 @@ describe("Signup Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
+
+  test("Should return 400 if no password is provided", async () => {
+    //system under test
+    const sut = new SignUpController();
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    const httpResponse: HttpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  });
+
+  test("Should return 400 if no password is provided", async () => {
+    //system under test
+    const sut = new SignUpController();
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+      },
+    };
+
+    const httpResponse: HttpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation")
+    );
+  });
 });
