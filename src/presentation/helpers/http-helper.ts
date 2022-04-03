d180a0704bedf020933034c8914b6fa75705b34d
deal-machine <95deal@gmail.com>
2022-04-02 23:50:31 -0300

    refactor: move all protocol imports to a single index file

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
 import { ServerError } from "../errors";
-import { HttpResponse } from "../protocols/http";
+import { HttpResponse } from "../protocols";
 
 export function badRequest(error: Error): HttpResponse {
   return { statusCode: 400, body: error };
