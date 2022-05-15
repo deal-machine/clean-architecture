8a961eed656faadf2b207361a21d7683c4d81eb2
deal-machine <95deal@gmail.com>
2022-05-15 00:18:18 -0300

    chore: increment tests scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,10 @@
 export default {
   roots: ["<rootDir>/src"],
-  collectCoverage: true,
+  testEnvironment: "node",
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
-  testEnvironment: "node",
 };
