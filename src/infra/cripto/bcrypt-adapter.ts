63235a9c5f8c156c6c53a3000437dde9b49d30c2
deal-machine <95deal@gmail.com>
2022-05-18 23:20:18 -0300

    refactor: clean spaces

--- src/infra/cripto/bcrypt-adapter.ts
+++ src/infra/cripto/bcrypt-adapter.ts
@@ -3,9 +3,11 @@ import { Encrypter } from "../../data/protocols/encrypter";
 
 export class BcryptAdapter implements Encrypter {
   private readonly salt: number;
+
   constructor(salt: number) {
     this.salt = salt;
   }
+
   async encrypt(value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt);
   }
