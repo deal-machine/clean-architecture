295be44a09b51cf7165ba2ca0769815141cf4edd
deal-machine <95deal@gmail.com>
2022-05-18 23:20:54 -0300

    test: ensure BcryptAdapter return hashed value

--- src/infra/cripto/bcrypt-adapter.spec.ts
+++ src/infra/cripto/bcrypt-adapter.spec.ts
@@ -1,13 +1,24 @@
 import bcrypt from "bcrypt";
 import { BcryptAdapter } from "./bcrypt-adapter";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash_value"));
+  },
+}));
+
+const salt = 12;
+
 describe("Bcrypt Adapter", () => {
   test("Should be call Bcryp with correct value", async () => {
-    const salt = 12;
     const sut = new BcryptAdapter(salt);
     const bcryptSpy = jest.spyOn(bcrypt, "hash");
-    await sut.encrypt("valid_value");
-
-    expect(bcryptSpy).toHaveBeenCalledWith("valid_value", salt);
+    await sut.encrypt("any_value");
+    expect(bcryptSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+  test("Should return hashed on success", async () => {
+    const sut = new BcryptAdapter(salt);
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toEqual("hash_value");
   });
 });
